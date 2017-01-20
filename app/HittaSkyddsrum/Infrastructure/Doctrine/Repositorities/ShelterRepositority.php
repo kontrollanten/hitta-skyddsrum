1769762a6a260c0a9bc1b96c78cccfdf7887d282
Alexander Wåland <alexander@width.se>
2017-01-20 15:48:42 +0100

    Order shelters by distance

--- app/HittaSkyddsrum/Infrastructure/Doctrine/Repositorities/ShelterRepositority.php
+++ app/HittaSkyddsrum/Infrastructure/Doctrine/Repositorities/ShelterRepositority.php
@@ -10,6 +10,8 @@
 namespace HittaSkyddsrum\Infrastructure\Doctrine\Repositorities;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use HittaSkyddsrum\Entities\Shelter;
 use HittaSkyddsrum\Infrastructure\Doctrine\Repositorities\Contracts\ShelterRepository as ShelterRepositoryContract;
 use HittaSkyddsrum\ValueObjects\Position;
@@ -25,27 +27,28 @@ class ShelterRepositority extends EntityRepository implements ShelterRepositoryC
 
     public function getCloseTo(Position $position, $distance)
     {
-        $qb = $this
-            ->getEntityManager()
-            ->createQueryBuilder();
+        $rsm = new ResultSetMapping();
+        $rsm->addEntityResult(Shelter::class, 's');
+        $rsm->addFieldResult('s', 'id', 'id');
 
-        $query = $qb
-            ->select(['s'])
-            ->from('HittaSkyddsrum\Entities\Shelter', 's')
-            ->where(
-                $qb->expr()->gte('s.position.lat', $position->getLat() - $distance)
-            )
-            ->andWhere(
-                $qb->expr()->lte('s.position.lat', $position->getLat() + $distance)
-            )
-            ->andWhere(
-                $qb->expr()->gte('s.position.long', $position->getLong() - $distance)
-            )
-            ->andWhere(
-                $qb->expr()->lte('s.position.long', $position->getLong() + $distance)
-            )
-            ->getQuery();
+        $rsm = new ResultSetMappingBuilder($this->getEntityManager());
+        $rsm->addRootEntityFromClassMetadata(Shelter::class, 's');
 
-        return $query->getResult();
+        $query = 'SELECT s.*, 
+( 3959 * acos( cos( radians(:lat) ) * 
+cos( radians( position_lat ) ) * 
+cos( radians( position_long ) - 
+radians(:lng) ) + 
+sin( radians(:lat) ) * 
+sin( radians( position_lat ) ) ) ) 
+AS distance FROM shelters s ORDER BY distance ASC LIMIT 0, 100';
+
+        $query = $this->getEntityManager()
+            ->createNativeQuery($query, $rsm)
+            ->setParameter('lng', '18.0887252')
+            ->setParameter('lat', '59.35330880000001');
+
+        return $query
+            ->getResult();
     }
 }
